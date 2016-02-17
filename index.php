f254710063292f51d58ca1954289a1eeecd7a057
lupete <lucasdasilva165@gmail.com>
2016-02-17 15:59:47 -0200

    Update index.php
    Adição do corpo

--- index.php
+++ index.php
@@ -9,10 +9,21 @@
       color:#FFF;
       width:100%;
     }
+    
+    #corpo{
+      width:80%;
+      height:1000px;
+      border:1px solid #F00;
+    }
   </style>
   <body>
     <div id="top">
-      Logo
+      <center>Logo</center>
+    </div><!--fim top-->
+    
+    <div id="corpo">
+      
     </div>
+    
   </body>
 </html>
